52b7ef980323a69aba02d3eab6ef6fee48ea5fba
hojong6636 <hojong6636@gmail.com>
2018-10-01 22:47:37 +0900

    add ::selection

--- app/styles/pageStyles.js
+++ app/styles/pageStyles.js
@@ -1,15 +1,21 @@
 import { css } from "styled-components";
 
 const style = css`
-  body {
-    @import url("https://fonts.googleapis.com/css?family=Lora:400,700|Work+Sans:300,400,700");
-  }
+  @import url("https://fonts.googleapis.com/css?family=Lora:400,700|Work+Sans:300,400,700");
   @media only screen and (min-width: 576px) {
     .home-col {
       top: -40%;
       position: absolute;
     }
   }
+  ::selection {
+    background-color: rgb(255, 240, 146);
+    color: #1106ff;
+  }
+
+  body {
+  }
+
   /* @media all and (max-width: 600px) {
     .home-col {
       color: blue;
